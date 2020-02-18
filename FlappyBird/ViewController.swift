8dc7c1a6d779ee2a9b3d8198a7e9551cb5c87dc9
genki-takami <takamigenki7@gmail.com>
2020-02-18 16:07:22 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by 髙見元基 on 2020/02/18.
+//  Copyright © 2020 genki.takami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
